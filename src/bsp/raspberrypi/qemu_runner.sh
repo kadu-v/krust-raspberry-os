9c7e17ce174b7723448c56d91aeb0407acee3429
kadu-v <kazuonsunday@yahoo.co.jp>
2022-01-09 18:22:20 +0900

    Add: 03-hacky hello world

--- src/bsp/raspberrypi/qemu_runner.sh
+++ src/bsp/raspberrypi/qemu_runner.sh
@@ -2,8 +2,8 @@
 
 # make the kernel image
 KERNEL_BIN=kernel8.img
-cargo objcopy --bin kernel -- --strip-all -O binary ./img/kernel8.img &&
+cargo objcopy --bin kernel --release -- --strip-all -O binary ./img/kernel8.img &&
 
 # laounch qemu
-qemu-system-aarch64 -M raspi3ap -d in_asm -display none -kernel ./img/kernel8.img
+qemu-system-aarch64 -M raspi3b -serial stdio -display none -kernel ./img/kernel8.img
 
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,20 @@
 #![no_std]
 #![no_main]
+#![feature(panic_info_message)]
 
 use core::panic;
 
 mod bsp;
+mod console;
 mod cpu;
 mod panic_wait;
+mod print;
 
 //-------------------------------------------------------------------------------------------------
 // Kernel code
 //-------------------------------------------------------------------------------------------------
 fn kernel_init() -> ! {
-    panic!()
+    println!("[0] Hello from Rust!");
+
+    panic!("Stopping here.");
 }
