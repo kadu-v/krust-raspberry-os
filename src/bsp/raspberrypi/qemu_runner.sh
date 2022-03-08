b38bf49c2b3c7d769c71caa95c270ffc2d503c7a
kadu-v <kazuonsunday@yahoo.co.jp>
2022-03-08 19:55:04 +0900

    Wip

--- src/bsp/raspberrypi/qemu_runner.sh
+++ src/bsp/raspberrypi/qemu_runner.sh
@@ -4,4 +4,4 @@
 cargo objcopy --bin kernel --release -- --strip-all -O binary ./img/kernel8.img &&
 
 # qemu-system-aarch64 -M raspi3 -serial stdio -display none -kernel ./img/kernel8.img
-qemu-system-aarch64 -M raspi3b -serial stdio -display none -kernel ./img/kernel8.img
+qemu-system-aarch64 -M raspi4 -serial stdio -kernel ./img/kernel8.img
