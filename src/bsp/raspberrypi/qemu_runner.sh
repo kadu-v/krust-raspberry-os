2d72168db6d540462cf4af2c7a3f34c995247ab6
kadu-v <kazuonsunday@yahoo.co.jp>
2026-07-02 23:20:05 +0900

    Update: run kernel on QEMU raspi4b with QEMU 11.0.2 built from source
    - Build latest QEMU (11.0.2) from source in Dockerfile, since the
      raspi4b machine requires QEMU >= 8.2 (Ubuntu 22.04 ships 6.2)
    - Switch default feature to bsp_rpi4 and target-cpu to cortex-a72
    - Use -M raspi4b in qemu_runner.sh and create img/ before objcopy

--- src/bsp/raspberrypi/qemu_runner.sh
+++ src/bsp/raspberrypi/qemu_runner.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 
+mkdir -p ./img &&
+
 cargo objcopy --bin kernel --release -- --strip-all -O binary ./img/kernel8.img &&
 
-# qemu-system-aarch64 -M raspi3b -serial stdio -display none -kernel ./img/kernel8.img
-qemu-system-aarch64 -M raspi3b -serial stdio -kernel ./img/kernel8.img
+# qemu-system-aarch64 -M raspi4b -serial stdio -display none -kernel ./img/kernel8.img
+qemu-system-aarch64 -M raspi4b -serial stdio -kernel ./img/kernel8.img
